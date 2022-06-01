2d660037353ef0870f6421bb07363c095da90d05
David Decottigny <ddecottigny@gmail.com>
2022-06-01 17:08:00 +0200

    Mise de l'application en argument du serveur

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,7 @@
 const http = require("http");
 
 //Création du serveur
-const server = http.createServer((req, res) => {
-  res.end("Le serveur fonctionne toujours !");
-});
+const server = http.createServer(app);
 
 //Mise en place de la méthode permettant au serveur d'écouter via les ports disponibles
 server.listen(process.env.port || 3000);
