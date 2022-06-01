c310c1f0a3f95764a44776d24973eb3ce7467410
David Decottigny <ddecottigny@gmail.com>
2022-06-01 17:01:42 +0200

    Export de l'application afin de pouvoir mettre l'application à disposition des autres fichiers, de pouvoir entre autres mettre l'application à disposition du serveur Node

--- backend/app.js
+++ backend/app.js
@@ -3,3 +3,6 @@ const express = require("express");
 
 // Création de l'application express
 const app = express();
+
+// Export de l'application afin de pouvoir notamment envoyer des requêtes au serveur
+module.exports = app;
