6c6abc76080adf0f2cd1d367c957919e7877dcda
David Decottigny <ddecottigny@gmail.com>
2022-06-06 16:06:46 +0200

    Mise en place des variables d'environnement terminée

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 // Installation et import du framework express
 const express = require("express");
 
-// Import du module de chargement des variables d'environnement
-const dotenv = require("dotenv");
-
 // Création de l'application express
 const app = express();
 
 // Export de l'application afin de pouvoir notamment envoyer des requêtes au serveur
 module.exports = app;
+
+// Import de la mécanique de connexion à la base de données
+const database = require("./config/database");
+
+// Test de connexion à la base de donnée
+try {
+  database.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,30 @@
-// Installation et import du package de l'ORM sequelize
-const sequelize = require("sequelize");
+const Sequelize = require("sequelize");
+
+// Chargement des variables d'environnement
+require("dotenv").config();
+
+// const dotenv = require("dotenv");
+// const dotenvConfig = dotenv.config();
+// console.log("dotenv");
+// console.log(dotenv);
+
+// console.log("dotenvConfig");
+// console.log(dotenvConfig);
+
+module.exports = new Sequelize(
+  process.env.DB,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    operatorsAliases: 0,
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
