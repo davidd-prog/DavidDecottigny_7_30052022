0e096d608c7723dfc98aa35b59ed8ef2f9f8e9ec
David Decottigny <ddecottigny@gmail.com>
2022-06-09 10:50:16 +0200

    Installation des packages npm bcrypt, jsonwebtoken et multer

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,10 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 // mécanique d'enregistrement d'un nouvel utilisateur
-exports.signup = (req, res, next) => {};
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then().catch();
+};
 
 // Mécanique de connexion de l'utilisateur
 exports.login = (req, res, next) => {};
