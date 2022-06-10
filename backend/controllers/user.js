ec385a07795f0db2c9189e0c29b69935906ed452
David Decottigny <ddecottigny@gmail.com>
2022-06-10 14:22:54 +0200

    Mise en place du middleware de connexion utilisateur avec la configuration du jsonwebtoken

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,6 +7,7 @@ exports.signup = (req, res, next) => {
     .hash(req.body.password, 10)
     .then((hash) => {
       const user = new User({
+        id: req.body.id,
         email: req.body.email,
         username: req.body.username,
         isadmin: req.body.isadmin,
@@ -21,4 +22,28 @@ exports.signup = (req, res, next) => {
 };
 
 // Mécanique de connexion de l'utilisateur
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user.id,
+            username: user.username,
+            token: jwt.sign({ userId: user.id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
+            message: "Utilisateur connecté !",
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
