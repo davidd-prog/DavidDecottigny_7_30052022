6f3b6254ae136f5758cf6ce59069c9f1df2de456
David Decottigny <ddecottigny@gmail.com>
2022-07-12 12:50:50 +0200

    middlewares de modification de post et de suppression de post opérationnels

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -54,13 +54,10 @@ exports.updateOnePost = (req, res, next) => {
       }
     : { ...req.body };
 
-  console.log(postObject);
-
   Post.findOne({ where: { id: req.params.id } })
     .then((post) => {
       if (post) {
         Post.update(
-          // { where: { id: req.params.id } },
           {
             ...postObject,
           },
@@ -74,67 +71,58 @@ exports.updateOnePost = (req, res, next) => {
       res.status(400).json({ error });
     });
 };
-// exports.updateOnePost = (req, res, next) => {
-//   Post.findOne({
-//     where: { id: req.params.id },
-//   }).then((post) => {
-//     if (!post) {
-//       return res.status(403).json({
-//         error: new Error("Post non trouvé"),
-//       });
-//     }
-//   });
-//   const postObject = req.file
-//     ? {
-//         // ...JSON.parse(req.body.post),
-//         // imageUrl: `${req.protocol}://${req.get("host")}/images/${
-//         //   req.file.filename
-//         // }`,
-//         ...req.body,
-//         image: `${req.protocol}://${req.get("host")}/images/${
-//           req.file.filename
-//         }`,
-//       }
-//     : { ...req.body };
-//   Post.update(
-//     { where: { id: req.params.id } },
-//     { ...postObject, where: { id: req.params.id } }
-//   )
-//     .then(() => res.status(200).json({ message: "Post mis à jour" }))
-//     .catch((error) => res.status(400).json({ error }));
-// };
 
 // Mécanique de suppression d'un post
+
 exports.deleteOnePost = (req, res, next) => {
   Post.findOne({ where: { id: req.params.id } })
     .then((post) => {
-      if (!post) {
-        return res.status(404).json({
-          error: new Error("Post non trouvé"),
+      if (post) {
+        // console.log("Le post est bien le même");
+        const filename = post.image.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+          Post.destroy({ where: { id: req.params.id } })
+            .then(() => res.status(200).json({ message: "Post supprimé" }))
+            .catch((error) => res.status(400).json({ error }));
         });
+      } else {
+        // console.log("Le post n'est pas le même");
+        return res.status(404).json({ error: new Error("Post non trouvé") });
       }
-      if (post.id !== req.auth.userId || user.isadmin != 1) {
-        return res.status(403).json({
-          error: new Error("Requête non autorisée"),
-        });
-      }
-      const filename = post.imageUrl.split("/images/")[1];
-      fs.unlink(`images/${filename}`, () => {
-        Post.destroy({ where: { id: req.params.id } })
-          .then(() =>
-            res.status(200).json({
-              message: "Post supprimé",
-            })
-          )
-          .catch((error) =>
-            res.status(400).json({
-              error,
-            })
-          );
-      });
     })
     .catch((error) => res.status(500).json({ error }));
 };
+
+// exports.deleteOnePost = (req, res, next) => {
+//   Post.findOne({ where: { id: req.params.id } })
+//     .then((post) => {
+//       if (!post) {
+//         return res.status(404).json({
+//           error: new Error("Post non trouvé"),
+//         });
+//       }
+//       // if (post.id !== req.auth.userId || user.isadmin != 1) {
+//       //   return res.status(403).json({
+//       //     error: new Error("Requête non autorisée"),
+//       //   });
+//       // }
+//       const filename = post.image.split("/images/")[1];
+//       fs.unlink(`images/${filename}`, () => {
+//         Post.destroy({ where: { id: req.params.id } })
+//           .then(() =>
+//             res.status(200).json({
+//               message: "Post supprimé",
+//             })
+//           )
+//           .catch((error) =>
+//             res.status(400).json({
+//               error,
+//             })
+//           );
+//       });
+//     })
+//     .catch((error) => res.status(500).json({ error }));
+// };
 
 // Mécanique d'évaluation d'un post
 exports.likePost = (req, res, next) => {};
