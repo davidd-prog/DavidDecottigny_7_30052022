b42bc0f90317941b97d6f80f611b20b0b34f547f
David Decottigny <ddecottigny@gmail.com>
2022-07-17 17:15:57 +0200

    Suppression commentaires inutiles dans config bdd

--- backend/config/database.js
+++ backend/config/database.js
@@ -33,21 +33,4 @@ try {
 } catch (error) {
   console.error("=> Impossible de se connecter à la base de données:", error);
 }
-
-// const db = {};
-
-// // Récupération de la connexion à la base de données et du package Sequelize
-// db.Sequelize = Sequelize;
-// db.sequelize = sequelize;
-
-// // Récupération des modèles de la base de données
-// db.user = require("../models/User")(sequelize, Sequelize);
-// db.post = require("../models/Post")(sequelize, Sequelize);
-
-// // Mise en relation des tables User et Post
-// db.user.hasMany(db.post);
-// db.post.belongsTo(db.user, { onDelete: "CASCADE" });
-// db.sequelize.sync();
-
-// module.exports = db;
- module.exports = sequelize;
+module.exports = sequelize;
